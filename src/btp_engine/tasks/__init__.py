61d13cf5085daacc52c3de67156154d946320817
philippe soulié <phippsou@hotmail.com>
2026-05-10 10:37:30 +0200

    fix: update tasks __init__.py imports

--- src/btp_engine/tasks/__init__.py
+++ src/btp_engine/tasks/__init__.py
@@ -1,7 +1,7 @@
 """Tasks module - Task generation and management."""
 
-from .task_generator import generate_tasks
-from .task_templates import get_task_template
-from .task_merger import merge_similar_tasks
+from .task_generator import TaskGenerator
+from .task_templates import TaskTemplates
+from .task_merger import TaskMerger
 
-__all__ = ["generate_tasks", "get_task_template", "merge_similar_tasks"]
+__all__ = ["TaskGenerator", "TaskTemplates", "TaskMerger"]
